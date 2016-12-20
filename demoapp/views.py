8fc7cbd61ae0c45794bad361c3ea338d908b28f9
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-19 20:14:20 -0800

    added views

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,6 +1,19 @@
-from pyramid.view import view_config
+from pyramid.response import Response
 
+def home(request):
+    return Response('/static/html-mockup/home.html')
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'demoapp'}
+def detail(request):
+    return Response('/static/html-mockup/detail.html')
+
+def create(request):
+    return Response('/static/html-mockup/edit_form.html')
+
+def update(request):
+    return Response('/static/html-mockup/create_form.html')
+
+def includeme(config):
+    config.add_view(home, '/'),
+    config.add_view(detail, '/journal/{id:\d+}'),
+    config.add_view(create, '/journal/new-entry'),
+    config.add_view(update, '/journal/{id:\d+}/edit-entry')
